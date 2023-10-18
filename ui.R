fe40508c4a488cf681e8ab4e76499a7fcaf5de86
Mason Davis <davism24@mail.wlu.edu>
2023-10-18 14:40:26 -0400

    formatting

--- ui.R
+++ ui.R
@@ -4,24 +4,13 @@ library(leaflet)
 
 murder_table <- readRDS("murders.RData")
 
-fluidPage(
-<<<<<<< HEAD
-  leafletOutput("StateMap"),
+fluidPage(leafletOutput("StateMap"),
   titlePanel("State-Map"),
-  sidebarLayout(
-    sidebarPanel,
-    mainPanel = mainPanel(plotOutput("StateMap"))
-=======
-<<<<<<< HEAD
-  titlePanel("Murders"),
-  
-=======
+  sidebarLayout(sidebarPanel,
+                mainPanel = mainPanel(plotOutput("StateMap"))),
   titlePanel("Murder Map"),
   leafletOutput("world_map"),
->>>>>>> 4ba11102e3e940a1f81cab36056bfa60d02f1b0b
-  sidebarLayout(
-    sidebarPanel (selectInput())
-    mainPanel = mainPanel(plotOutput("##"))
->>>>>>> a38f4208a50fdcf2f43fb28bc295bc415c9ad3d2
+  sidebarLayout(sidebarPanel (selectInput()),
+  mainPanel = mainPanel(plotOutput("##"))
   )
 )
