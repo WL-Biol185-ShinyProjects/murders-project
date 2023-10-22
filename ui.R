3fea2e0cddf12c0211467bb7e18a5c680a7d0c7b
Gretta <syrettg24@mail.wlu.edu>
2023-10-22 11:25:08 -0400

    Maybe map

--- ui.R
+++ ui.R
@@ -5,12 +5,11 @@ library(leaflet)
 murder_table <- readRDS("murders.RData")
 
 fluidPage(
-  titlePanel("StateMap"),
-  # mainPanel = mainPanel(plotOutput("StateMap")),
-  leafletOutput("StateMap", width = "100%", height = "100%")
+  titlePanel("StateMap"), #Puts words stateMap on page
+  mainPanel(
+    leafletOutput(outputId = 'map', width = "100%", height = "100%") #Creates a map
   )
-<<<<<<< HEAD
+  
+  
 )
 
-=======
->>>>>>> d8da37a5a1c5b5204ac13e0a9ea91fd632a638a2
