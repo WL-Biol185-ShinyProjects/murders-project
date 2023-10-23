ba5dbb5af831ef2c1c6a4d630c49cde64ba8406a
Mason Davis <davism24@mail.wlu.edu>
2023-10-23 15:01:20 -0400

    combining servers

--- server.R
+++ server.R
@@ -39,6 +39,26 @@ function(input, output, session) {
         )
       })
     })
+  
+  #Subsitute dataname before $ then column name after
+  output$map <- renderLeaflet({ #Creating the map
+    murderColorGradient <- colorNumeric("Reds", domain=DATANAME$COLUMNANAME) #Sets variable that assigns colors of red, heavier red = more murders. Use data name $ column name of the murders
+    leaflet(data= murderDataCHANGE) #Where data goes when you make it
+    addTiles() %>% #Makes map theme
+      setView(0, 40, zoom =4) %>% #Sets zoom doesnt matter 
+      
+      addPolygons( 
+        fillColor = ~murderColorGradient(DATANAE$COLNAME), 
+        fillOpacity = 0.7, #Play with these as needed
+        weight = 0.2, 
+        smoothFactor = 0.2) %>%
+      addMarkers(
+        lng = ~Longitude,
+        lat = ~Latitude,
+        label = finalDogData$Breed,
+        
+      )
+  })
 }
 
 
