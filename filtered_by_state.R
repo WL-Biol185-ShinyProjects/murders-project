e4a3e135e3e2a38fe65c31b7d0243f074e51cedf
Lucy <worthyl24@mail.wlu.edu>
2023-10-25 16:36:15 -0400

    found most common weapon in alabama

--- filtered_by_state.R
+++ filtered_by_state.R
@@ -5,8 +5,7 @@ library(tidyverse)
 
 Alabama <- murder_table %>%
   filter(State == "Alabama")
-  # summarise(total_murders = n()) %>%
 
 Alabama_weapon <- Alabama[, -c(2:12)] 
-  common_weapon <- unique(Alabama_weapon$Weapon, incomparables = FALSE)
-  print(common_weapon)
+  names(which.max(table(Alabama_weapon$Weapon)))
+
