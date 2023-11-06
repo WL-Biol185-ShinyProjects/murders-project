4b974d7ce6a715bcf9f3d7d42baf1b7b324d762a
Anna Marie Harding <hardinga25@mail.wlu.edu>
2023-11-06 16:21:00 -0500

    trying out ui

--- murder_table.R
+++ murder_table.R
@@ -1,5 +1,12 @@
 ##Calling initial data set
 murder_table <- readRDS("murders.RData")
+
+##Filtering data set for slider
+filteredData <- reactive({
+  murder_table[murder_table$Year >= input$range[1] & murder_table$Year <= input$range[2],]
+})
+
+##Merging with geojson
 geo <- geojson_read("states.geo.json", 
                     what = "sp")
 
@@ -8,3 +15,4 @@ incident_per_state <- murder_table %>%
   group_by(State) %>%
   summarise(total_murders = n()) %>%
   filter(!row_number() %in% c(52))
+
