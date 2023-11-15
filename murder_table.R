a5eb855e93024619dfb1b3202f6f982d1ae80e43
Mason Davis <davism24@mail.wlu.edu>
2023-11-15 15:52:39 -0500

    all members edits in class and before class

--- murder_table.R
+++ murder_table.R
@@ -1,5 +1,5 @@
 ##Calling initial data set
-murder_initial <- readRDS("murders.RData")
+murder_table <- readRDS("murders.RData")
 
 #Fixing spelling error in data set 
 library(stringr)
@@ -8,10 +8,4 @@ murder_table$State <- str_replace_all(murder_table$State, "Rhodes Island", "Rhod
 ##Merging with geojson
 geo <- geojson_read("states.geo.json", 
                     what = "sp")
-
-##Filtering data
-# incident_per_state <- murder_table %>%
-#   group_by(State) %>%
-#   summarise(total_murders = n()) %>%
-#   filter(!row_number() %in% c(52))
 
