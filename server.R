4151bddd4ef40405d70d46cabe7fdc427d131a77
Anna Marie Harding <hardinga25@mail.wlu.edu>
2023-11-29 16:50:39 -0500

    getting rid of nots -- all 4

--- server.R
+++ server.R
@@ -17,8 +17,6 @@ source("ggplots.R")
 qpal <- colorQuantile("Reds",
                 popup_data$total_murders,
                 n = 7)
-#changed from murder_table to CENSUSAREA line 16 after $
-#got an error of "memory allocation error"... memory.limit(100) possible fix ***ASK Whitworth 
 
 ##Drawing the map   
 function(input, output, session) {
